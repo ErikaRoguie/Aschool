ca56b3310c70424bb39dfde3a121b44ad3aaaa12
Erika <88861951+ErikaRoguie@users.noreply.github.com>
2021-11-02 14:41:53 -0700

    Add files via upload

---
+++ modules/Watch-that-box.javascript.js
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Watch That box</title>
+</head>
+<body>
+
+    <p>Press the buttons to move the box!</p>
+
+    <div id="box" style="height:150px; width:150px; background-color:orange; margin:25px"></div>
+
+    <button id="shrinkBtn">Shrink</button>
+    <button id="growBtn">Grow</button>
+    <button id="resetBtn">Reset</button>
+    <button id="button1">Grow</button>
+    <button id="button2">Blue</button>
+    <button id="button3">Fade</button>
+    <button id="button4">Reset</button>
+
+    <script type="text/javascript" src="javascript.js"></script>
+
+    </script>
+
+</body>
+</html>
---
+++ modules/fanpage/fanpage/Fanpage.html.cpp
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Disney Records</title>
+    <link rel="Disney Music" href="https://www.disneymusicemporium.com/">
+    <a href="https://music.amazon.com/search/disney?filter=IsLibrary%7Cfalse&sc=none">Sounds of Disney</a>
+    </head>
+    <body>
+    <p><h1>WELCOME TO DISNEY MUSIC EMPORIUM!</h1>
+<h2>Disney Music Emporium is THE online destination for all things collectible and limited edition from the legendary Disney Music catalog.
+Disney Music Emporium's (DME) goal is not only to create a marketplace where our fans can access unique and collectible music products, but to also create a space where we can engage together in a dynamic and exciting way. DME is Disney Music Group's "Field of Dreams," a destination to showcase unique and collectible Disney music products. We have a rich catalog of music that continues to grow year after year and our genuine hope is that DME becomes a favorite spot for you to interact with our catalog and join us in conversation.
+We want your input! We value your expertise and enthusiasm to help ensure our great catalog of music is always accessible for all to enjoy. If you have a great idea of a product you would like to see, we would love to hear from you. Please contact us at DMGDMEfeedback@disney.com. We will do our best to respond in a timely manner. For immediate customer service or order issues please see the “Questions or Concerns" section below.
+We look forward to hearing from you!
+Sincerely,
+The Disney Music Emporium Team </h2></p>
+    </body>
+
